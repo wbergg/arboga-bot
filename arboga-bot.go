f5c55173d1093986bc3a345e3dc58365bd6cefcf
wberg <wberg@wberg.com>
2020-01-21 21:47:02 +0100

    struct for request

--- arboga-bot.go
+++ arboga-bot.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"bytes"
-	"encoding/json"
 	"fmt"
 	"io/ioutil"
 	"net/http"
+
+	"github.com/go-redis/redis"
 )
+
+type Request struct {
+	ProductID string   `json:"productId"`
+	SiteIds   []string `json:"siteIds"`
+}
 
 type Response []struct {
 	SiteID            string `json:"SiteId"`
@@ -27,10 +33,26 @@ func main() {
 }
 
 func requestData() {
+	client := redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // no password set
+		DB:       0,  // use default DB
+	})
+
+	_, err := client.Ping().Result()
+	if err != nil {
+		panic(err)
+	}
 
 	s := Response{}
+	//json := Request{}
 
-	jsonValue := []byte(`{"productId":"508393","siteIds":["0611"]}`)
+	//jsonValue := []byte(`{"productId":"508393","siteIds":["0611"]}`)
+	jsonValue = json.Marshal(Request{ProductID:"508393",SiteIds:["0611"]})
+	//jsonValue, err := json.Marshal(json)
+	//if err != nil {
+	//	panic(err)
+	//}
 
 	res, err := http.Post("https://www.systembolaget.se/api/product/getstockbalance", "application/json", bytes.NewBuffer(jsonValue))
 	if err != nil {
@@ -46,7 +68,34 @@ func requestData() {
 
 	json.Unmarshal(body, &s)
 
-	//fmt.Println(string(body))
-
 	fmt.Println(s[0].StockTextShort)
+
+	//Prepare Telegram message
+	message := ""
+	message = message + "*DEKADENS UPDATE 2000!*\n"
+	message = message + "https://www.entremalmo.se/wp-content/uploads/2017/11/systembolaget.png" + "\n"
+	message = message + "Someone just bought an Arboga 10.2 @ Systembolaget in Gislaved!\n"
+
+	for _, site := range s {
+
+		val, err := client.Get(site.SiteID).Result()
+		if err != nil {
+			panic(err)
+		}
+
+		if val == site.StockTextShort {
+			fmt.Println("no change")
+		} else {
+			err = client.Set(site.SiteID, site.StockTextShort, 0).Err()
+			if err != nil {
+				panic(err)
+			}
+		}
+
+		message = message + "Currently left in stock: " + s[0].StockTextShort + "\n"
+
+	}
+	// Send message to Telegram
+	//t.SendM(message)
+	fmt.Println(message)
 }
